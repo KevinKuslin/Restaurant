177391ee12fba969d772e4edfc734899c45d5202
KevinKuslin <tyrantnitrox@gmail.com>
2025-03-20 23:54:36 +0700

    restaurant push

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,25 @@
+package restaurant;
+import main.Food; 
+import java.util.*; 
+
+public class Menu {
+	private String name; 
+	private ArrayList<Food> foods = new ArrayList<>(); 
+	
+	public String getName() {
+		return name; 
+	}
+	
+	public List<Food> getFood(){
+		return foods; 
+	}
+	
+	
+	public Menu(String name) {
+		this.name = name; 
+	}
+	
+	public void add(Food f) {
+		foods.add(f); 
+	}
+}
